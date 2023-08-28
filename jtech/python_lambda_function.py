dae9a7d482ca84bfc5c70ca58e04dea47588b426
Abbeydauda20 <abbeydauda20@gmail.com>
2023-08-28 18:45:03 -0400

    added new comments

--- jtech/python_lambda_function.py
+++ jtech/python_lambda_function.py
@@ -1,6 +1,6 @@
 """
-lambda function - function without defining names
-
+lambda function - function without defining names, useful to do a simple quick operation without any complex requirement
+you can write a lambda function and pass in the values
 
 """
 
@@ -12,3 +12,9 @@ lambda function - function without defining names
 
 add = lambda x, y: x + y
 print(add(10,20))
+
+output = lambda x, y: x-y
+print(output(20,10))
+
+output = lambda x, y: x/y
+print(output(20,10))
